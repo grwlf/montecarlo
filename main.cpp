8a54391e4dd24269f0b31386d1115e2f2ebb085f
Dmitry Vyal <akamaus@gmail.com>
2016-03-23 20:14:47 +0300

    more methods moved to Board, game loop

--- main.cpp
+++ main.cpp
@@ -19,7 +19,15 @@ constexpr int EPS = 100000; // 10%-greedy
     FOR_EACH_LOOP(i,j)                                  \
         if (b.board[i][j] == Board::cell::Empty)
 
+struct action {
+  int i; // 0..2
+  int j; // 0..2
+};
+
+enum class MoveResult { Draw, Win, Continue };
+
 struct Board {
+public:
     enum class cell {X, O, Empty};
     static constexpr int size = 3;
 
@@ -28,48 +36,73 @@ struct Board {
             board[i][j] = cell::Empty;
         }
     }
+    MoveResult Move(action a, Board::cell who) {
+        assert(board[a.i][a.j] == Board::cell::Empty);
+        board[a.i][a.j] = who;
+        num_empties--;
 
-  cell board[size][size];
+        auto &b = board;
+
+        for(int i=0; i<3; i++) {
+            if(b[0][i] == b[1][i] && b[1][i] == b[2][i] && b[2][i] == who)
+                return MoveResult::Win;
+        }
+        for(int i=0; i<3; i++) {
+            if(b[i][0] == b[i][1] && b[i][1] == b[i][2] && b[i][2] == who)
+                return MoveResult::Win;
+        }
+        if(b[0][0] == b[1][1] && b[1][1] == b[2][2] && b[2][2] == who)
+            return MoveResult::Win;
+        if(b[0][2] == b[1][1] && b[1][1] == b[2][0] && b[2][0] == who)
+            return MoveResult::Win;
+
+        if (num_empties == 0) {
+            return MoveResult::Draw;
+        }
+        return MoveResult::Continue;
+    }
+    cell At(int i, int j) const {
+        assert(i >=0 && i < size);
+        assert(j >=0 && j < size);
+        return board[i][j];
+    }
+    void Print() const {
+        FOR_EACH_LOOP(i,j) {
+            const char *c;
+            switch(At(i,j)) {
+            case cell::X:
+                c = "X";
+                break;
+            case cell::O:
+                c = "O";
+                break;
+            case cell::Empty:
+                c = ".";
+                break;
+            };
+            fprintf(stdout,"%s%s", c, (j == size-1) ? "\n":"");
+        }
+        fprintf(stdout, "\n");
+    }
+    int NumEmpties() const { return num_empties; }
+    cell board[size][size];
+private:
+    int num_empties = size*size;
 };
-
-struct action {
-  int x; // 0..2
-  int y; // 0..2
-};
-
-bool move ( Board &bo, Board::cell who, action a) {
-  bo.board[a.x][a.y] = who;
-
-#define b bo.board
-
-  for(int i=0; i<3; i++) {
-    if(b[0][i] == b[1][i] && b[1][i] == b[2][i] && b[2][i] == who)
-      return true;
-  }
-  for(int i=0; i<3; i++) {
-    if(b[i][0] == b[i][1] && b[i][1] == b[i][2] && b[i][2] == who)
-      return true;
-  }
-  if(b[0][0] == b[1][1] && b[1][1] == b[2][2] && b[2][2] == who)
-    return true;
-  if(b[0][2] == b[1][1] && b[1][1] == b[2][0] && b[2][0] == who)
-    return true;
-
-#undef b
-}
 
 namespace std {
 
     template <>
     struct hash<action> {
         size_t operator()(const action& a) const {
-            return a.x + a.y * 43;
+            return a.i + a.j * 43;
         }
     };
 
     bool operator==(const Board &b1, const Board &b2) {
+        if (b1.NumEmpties() != b2.NumEmpties()) return false;
         FOR_EACH_LOOP(i,j) {
-            if (b1.board[i][j] != b2.board[i][j]) {
+            if (b1.At(i,j) != b2.At(i,j)) {
                 return false;
             }
         }
@@ -112,48 +145,74 @@ private:
     int n;
 };
 
-struct ActionStats {
-    action act;
-    Averager stats;
-};
-
 struct policy {
-  unordered_map<Board, ActionStats> actmap;
+  unordered_map<Board, action> actmap;
 };
 
 action sample_policy( const policy& p, const Board &b )
 {
-    int num_empties = 0;
-    FOR_EACH_EMPTY(b) {
-        num_empties++;
-    }
-
     const auto &p_act = p.actmap.find(b);
 
     if (p_act == p.actmap.end() || (rand() % (1 << 20)) < EPS) {
-        int n = rand() % num_empties;
+        int n = rand() % b.NumEmpties();
         FOR_EACH_EMPTY(b) {
             if (n == 0) return {i,j};
             else n--;
         }
         assert(false);
     } else {
-        return p_act->second.act;
+        return p_act->second;
     }
 }
 
+typedef pair<Board, action> StateAct;
+
 struct Q {
-  unordered_map< pair<Board, action>, double > qmap;
+  unordered_map< StateAct, Averager > qmap;
 };
 
 void driver(policy &player1, policy &player2) {
+    Q q1;
+    Q q2;
+
+    vector<StateAct> episode1;
+    vector<StateAct> episode2;
     while(true) { // episode
         Board b;
+        episode1.clear();
+        episode2.clear();
+
+        int reward = 0;
+        while(true) {
+            action a1 = sample_policy(player1, b);
+            MoveResult res = b.Move(a1, Board::cell::X);
+            b.Print();
+            if (res == MoveResult::Win) {
+                reward = 1;
+                break;
+            } else if (res == MoveResult::Draw) {
+                reward = 0;
+                break;
+            }
+            action a2 = sample_policy(player2, b);
+            res = b.Move(a2, Board::cell::O);
+            b.Print();
+            if (res == MoveResult::Win) {
+                reward = -1;
+                break;
+            } else if (res == MoveResult::Draw) {
+                reward = 0;
+                break;
+            }
+        }
+        break;
     }
 }
 
 int main() {
-  return 0;
+    policy p1, p2;
+    driver(p1,p2);
+    return 0;
 }
 
 
